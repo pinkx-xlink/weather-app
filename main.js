62e2fa0194565670c6b967fd32a6036f1c0afaea
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-06-12 20:25:37 -0400

    Add a try...catch statement

--- main.js
+++ main.js
@@ -3,4 +3,8 @@ const url = 'https://api.weatherapi.com/v1/current.json?key=48e6d0ed95094ce58d71
 fetch('https://api.weatherapi.com/v1/current.json?key=48e6d0ed95094ce58d710855232908&q=london')
   .then(response => response.json())
   .then(data => console.log(data));
-  
+  try {
+    // nonexistantFunction();
+  } catch (error) {
+    console.log(error)
+  }
