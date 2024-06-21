de9158cac8af84c56d4e1442657ffebf7e0d030f
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-06-20 20:56:09 -0400

    Refactor data-container; create it with HTML at the top of the page, and simply update the innerHTML with JavaScript

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const url = 'https://api.weatherapi.com/v1/current.json?key=48e6d0ed95094ce58d710855232908&q=chicago';
 
-const dataContainer = document.createElement('div');
-dataContainer.innerHTML = 'hi';
-document.body.appendChild(dataContainer);
+const dataContainer = document.querySelector('.data-container');
 
 fetch('https://api.weatherapi.com/v1/current.json?key=48e6d0ed95094ce58d710855232908&q=chicago')
   .then(response => response.json())
