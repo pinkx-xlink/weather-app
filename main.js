4dea3ec4fe57dd52376fe526fe24f1b54a75a210
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-06-21 13:59:06 -0400

    Update comment on line 6, and fetch the current forecast (min and high temps)

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const url = 'https://api.weatherapi.com/v1/current.json?key=48e6d0ed95094ce58d71
 const city = document.querySelector('.city');
 const temp = document.querySelector('.temp');
 const currentTempCard = document.getElementById('forecast-today');
-// Fetch the CURRENT weather info
+// Fetch the CURRENT temp
 fetch('https://api.weatherapi.com/v1/current.json?key=48e6d0ed95094ce58d710855232908&q=chicago')
   .then(response => response.json())
   .then(data => {
@@ -28,8 +28,16 @@ try {
 } catch (error) {
   console.log(error);
 }
+// Fetch the current forecast (min and high temps)
+fetch('https://api.weatherapi.com/v1/forecast.json?key=48e6d0ed95094ce58d710855232908&q=chicago')
+  .then(response => response.json())
+  .then(data => {console.log(data);})
+
 
 // Fetch tomorrow's forecast
+
+
+
 
 // Fetch the current astronomy
 fetch('https://api.weatherapi.com/v1/astronomy.json?key=48e6d0ed95094ce58d710855232908&q=chicago')
