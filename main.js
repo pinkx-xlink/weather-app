f8d7adae5c4fadc1ff70ca24dde7dce2c5ae6a40
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-06-20 20:13:21 -0400

    Change default city to chicago

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const url = 'https://api.weatherapi.com/v1/current.json?key=48e6d0ed95094ce58d710855232908&q=london';
+const url = 'https://api.weatherapi.com/v1/current.json?key=48e6d0ed95094ce58d710855232908&q=chicago';
 
-fetch('https://api.weatherapi.com/v1/current.json?key=48e6d0ed95094ce58d710855232908&q=london')
+fetch('https://api.weatherapi.com/v1/current.json?key=48e6d0ed95094ce58d710855232908&q=chicago')
   .then(response => response.json())
   .then(data => console.log(data));
   try {
