1a19c336cc37680deaffd70b19f64170692db1ee
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-06-20 20:28:23 -0400

    Create dataContainer div

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const url = 'https://api.weatherapi.com/v1/current.json?key=48e6d0ed95094ce58d710855232908&q=chicago';
 
+const dataContainer = document.createElement('div');
+dataContainer.innerHTML = 'hi';
+document.body.appendChild(dataContainer);
+
 fetch('https://api.weatherapi.com/v1/current.json?key=48e6d0ed95094ce58d710855232908&q=chicago')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+    console.log(data)
+  })
   try {
-    // nonexistantFunction();
+  // nonexistantFunction();
   } catch (error) {
     console.log(error)
   }
