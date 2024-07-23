e921a66c1b91e00abe9922436bf0021e4396e871
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-07-23 15:08:46 -0400

    Add code to (try to) update the two day forecast based on the searched city (not functioning properly yet)

--- main.js
+++ main.js
@@ -102,15 +102,61 @@ submit.addEventListener('click', function searchCity(e) {
       <h2>${currentTempF}℉</h2>
       <p>${weatherDescription}</p>
     `;
-    // NEED TO UPDATE FORECAST FOR TOMORROW AND NEXT DAY
-    // ONCE SEARCH IS SUBMITTED
     });
+
+// NEED TO UPDATE FORECAST FOR TOMORROW AND NEXT DAY
+// ONCE SEARCH IS SUBMITTED
+
+    fetch('https://api.weatherapi.com/v1/current.json?key=48e6d0ed95094ce58d710855232908&q='+`${search}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.forecast);
+    const tempHigh = data.forecast.forecastday[0].day.maxtemp_f;
+    const tempLow = data.forecast.forecastday[0].day.mintemp_f;
+    currentTempCard.innerHTML += `
+    <p>High: ${tempHigh}℉</p>
+    <p>Low: ${tempLow}℉</p>
+    `;
+    const tmrwAverageTemp = data.forecast.forecastday[1].day.avgtemp_f;
+    const tmrwHighTemp = data.forecast.forecastday[1].day.maxtemp_f;
+    const tmrwLowTemp = data.forecast.forecastday[1].day.mintemp_f;
+    const tmrwWeatherDescription = data.forecast.forecastday[1].day.condition.text;
+    const tmrwWeatherIcon = data.forecast.forecastday[1].day.condition.icon;
+    tomorrowTempCard.innerHTML = `
+    <h1> ${tmrwDayName} </h1>
+    <img src=${tmrwWeatherIcon} /img>
+    <h2>${tmrwAverageTemp}℉</h2>
+    <p>${tmrwWeatherDescription}</p>
+    <p>High: ${tmrwHighTemp}℉</p>
+    <p>Low: ${tmrwLowTemp}℉</p>
+    `;
+    const twoDayAverageTemp = data.forecast.forecastday[2].day.avgtemp_f;
+    const twoDayHighTemp = data.forecast.forecastday[2].day.maxtemp_f;
+    const twoDayLowTemp = data.forecast.forecastday[2].day.mintemp_f;
+    const twoDayWeatherDescription = data.forecast.forecastday[2].day.condition.text;
+    const twoDayWeatherIcon = data.forecast.forecastday[2].day.condition.icon;
+    console.log(`In ${currentCity} it is ${twoDayHighTemp} `)
+    twoDayForecast.innerHTML = `
+    <h1> ${inTwoDaysDayName} </h1>
+    <img src=${twoDayWeatherIcon} /img>
+    <h2>${twoDayAverageTemp}℉</h2>
+    <p>${twoDayWeatherDescription}</p>
+    <p>High: ${twoDayHighTemp}℉</p>
+    <p>Low: ${twoDayLowTemp}℉</p>
+    `;
+    });
+    // END OF updating future forecast for searched city
   try {
   // nonexistantFunction();
   } catch (error) {
     console.log(error);
   }
 });
+
+
+
+
+
 // Fetch the current forecast (min and high temps)
 fetch('https://api.weatherapi.com/v1/forecast.json?key=48e6d0ed95094ce58d710855232908&q=chicago&days=3')
   .then(response => response.json())
